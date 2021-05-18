c29ac9c8bf55924e375a2c2ad62b0e346d86435f
karankochar <fundookaran@gmail.com>
2021-05-18 18:24:38 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import Data from './booklist.json';
+import { useState } from 'react';
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      { 
+        <table className="table table-bordered">
+         <thead>
+           <tr>
+           <th>Book Id</th>
+           <th>Book Title</th>
+           <th>Book Author</th>
+           <th>Book Year of publish</th>
+           </tr>
+           <tr>
+           <th><input type='text' 
+                placeholder='Search By Id' 
+                onChange={(e)=> {setSearchTerm(e.target.value)}}>
+                </input></th>
+           <th><input type='text' 
+                placeholder='Search By Title' 
+                onChange={(e)=> {setSearchTerm(e.target.value)}}>
+                </input>
+          </th>
+           <th><input type='text' 
+                placeholder='Search By Author' 
+                onChange={(e)=> {setSearchTerm(e.target.value)}}>
+                </input></th>
+           <th><input type='text' 
+                placeholder='Search By Year' 
+                onChange={(e)=> {setSearchTerm(e.target.value)}}>
+                </input></th>
+           </tr>
+          </thead>
+          <tbody>
+            {Data.filter((book)=>{
+              if(searchTerm ===''){
+                return book
+              }
+              else if(book.title.toLowerCase().includes(searchTerm.toLowerCase())){
+                return book;
+              }
+              else if(book.author.toLowerCase().includes(searchTerm.toLowerCase())){
+                return book;
+              }
+              else if(book.id == searchTerm){
+                return book;
+              }
+              else if(book.year == searchTerm){
+                return book;
+              }
+            }).map(book=> {
+              return(
+                <tr>
+                <td>{book.id}</td>
+                <td>{book.title}</td>
+                <td>{book.author}</td>
+                <td>{book.year}</td>
+              </tr>
+              );
+              
+            })}
+          </tbody>
+       </table>
+
+      }
     </div>
   );
 }
